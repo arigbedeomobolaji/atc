e62698d756ef0c9a1add556a51e9a8387a129cbf
Arigbede Omobolaji <arigbedeomobolaji@gmail.com>
2026-01-06 17:38:20 +0100

    fixing params/promise bug

--- src/app/api/news/image/[id]/route.ts
+++ src/app/api/news/image/[id]/route.ts
@@ -1,37 +1,3 @@
-// // app/api/news/image/[id]/route.ts
-
-// import { connectToDatabase } from "@/lib/db";
-// import { ObjectId } from "mongodb";
-
-// export async function GET(
-//   req: Request,
-//   { params }: { params: { id: string } }
-// ) {
-//   try {
-//     const { id } = params;
-//     const { db } = await connectToDatabase();
-//     const bucket = new (await import("mongodb")).GridFSBucket(db, {
-//       bucketName: "news_images",
-//     });
-//     const _id = new ObjectId(id);
-
-//     // find file info
-//     const files = await db.collection("news_images.files").findOne({ _id });
-//     if (!files) return new Response("Not found", { status: 404 });
-
-//     const downloadStream = bucket.openDownloadStream(_id);
-
-//     return new Response(downloadStream, {
-//       headers: {
-//         "Content-Type": files.contentType || "application/octet-stream",
-//       },
-//     });
-//   } catch (err) {
-//     console.error(err);
-//     return new Response("Server error", { status: 500 });
-//   }
-// }
-
 // app/api/news/image/[id]/route.ts
 
 import { connectToDatabase } from "@/lib/db";
@@ -39,7 +5,7 @@ import { ObjectId } from "mongodb";
 
 export async function GET(
   req: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
     const { id } = await params;
